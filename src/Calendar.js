f7c7dd66efb6daa4bd1a347ebdd1bc88bbc25327
han-bom <livet@kakao.com>
2020-10-05 13:35:29 +0900

    달력 기본 변수 생성

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Calendar() {
+    const [date, setDate] = useState(new Date());
+    const [startDay, setStartDay] = useState(0);
+    const [endDate, setEndDate] = useState(0);
+
+    useEffect(() => {
+        let tmpDate = new Date(date.getTime());
+
+        tmpDate.setDate(1);
+        setStartDay(tmpDate.getDay());
+
+        tmpDate.setMonth(date.getMonth() + 1, 0);
+        setEndDate(tmpDate.getDate());
+    }, [date])
+
     return (
         <div className="Calendar">
-
+            {startDay}
+            {endDate}
         </div>
     )
 }
