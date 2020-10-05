270d28da6559f9b516b83d6c0eef5b7cb52bf7e3
han-bom <livet@kakao.com>
2020-10-05 15:18:10 +0900

    스케쥴러 버튼추가

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -3,9 +3,15 @@ import STR_DAY from './define';
 
 function Scheduler(props) {
 	const selectedDate = props.selectedDate;
+    const [isShowForm, setShowForm] = useState(false);
 
+    const handleClose = () => setShowForm(false);
+    
     return (
         <div className="Scheduler">
+            {STR_DAY[selectedDate.getDay()] + ' ' + selectedDate.getDate()}
+            <button onClick={() => setShowForm(true)}>Add</button>
+            <button>Clear All</button>
         </div>
     )
 }
