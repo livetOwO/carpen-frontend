a0c720bf42727c676f59b411d8a686eaa0c06d34
han-bom <livet@kakao.com>
2020-10-05 16:18:00 +0900

    로컬스토리지

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -1,22 +1,36 @@
-import React, {useState } from 'react';
+import React, {useEffect, useState } from 'react';
 import STR_DAY from './define';
 import AddSchedule from './AddSchedule';
 
 function Scheduler(props) {
 	const selectedDate = props.selectedDate;
     const [isFormShow, setFormShow] = useState(false);
+    const [schedule, setSchedule] = useState([]);
 
+    const dateToString = date => date.toISOString().substr(0, 10);
     const handleClose = () => setFormShow(false);
-    
+
     function handleAdd(time, work) {
         let data = {
-            date: selectedDate.toISOString().subString(0,10),
+            date: dateToString(selectedDate),
             time,
             work
         }
 
-        console.log(JSON.stringify(data));
+        schedule.push(data);
+
+        localStorage.setItem(JSON.stringify(schedule));
+
+        setSchedule(schedule);
     }
+
+    useEffect(() => {
+        let tmp = localStorage.getItem('livet_scheduler');
+        
+        if (tmp) {
+            setSchedule(JSON.parse(tmp));
+        }
+    }, [schedule]);
 
     return (
         <div className="Scheduler">
