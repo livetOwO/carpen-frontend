ae900471a882162a85cc59af6f611eb894011786
han-bom <livet@kakao.com>
2020-10-05 13:46:05 +0900

    날짜 표시

--- src/Calendar.js
+++ src/Calendar.js
@@ -24,6 +24,30 @@ function Calendar() {
                         {STR_DAY.map((day, i) => <th key={i}>{day}</th>)}
                     </tr>
                 </thead>
+                <tbody>
+                    {Array(6).fill(0).map((v,y) => (
+                        <tr>
+                            {Array(7).fill(0).map((v, x) => {
+                                let num = y * 7 + x - startDay + 1;
+                                let selected = false;
+
+                                // if (
+                                //     selectedDate.getFullYear() === date.getFullYear() &&
+                                //     selectedDate.getMonth() === date.getMonth() &&
+                                //     selectedDate.getDate() === num
+                                // ) {
+                                //     selected = true;
+                                // }
+
+                                if (num > 0 && num <= endDate) {
+                                    return <td key={x} className={selected ? 'selected' : undefined}>{num}</td>
+                                } else {
+                                    return <td key={x}></td>
+                                }
+                            })}
+                        </tr>
+                    ))}
+                </tbody>
             </table>
         </div>
     )
