9716cdac373034f14f86de5b935520e20a4234e8
han-bom <livet@kakao.com>
2020-10-05 15:26:30 +0900

    스케쥴 폼 추가

--- src/AddSchedule.js
+++ src/AddSchedule.js
@@ -4,8 +4,22 @@ function AddSchedule(props) {
 	const [time, setTime] = useState(undefined);
 	const [work, setWork] = useState(undefined);
 
+	const close = () => props.close();
+
+	function submit(e) {
+		e.preventDefault();
+
+		close();
+	}
+
 	return (
 		<div className="AddSchedule">
+			<form action="#AddSchedule" onSubmit={submit}>
+				<label><span>time</span> <input type="time" value={time} onChange={e => setTime(e.target.value)} /></label>
+				<label><span>work</span> <input type="text" value={work} onInput={e => setWork(e.target.value)} /></label>
+				<input type="submit" value="Add" />
+				<input type="button" value="Cancel" onClick={close} />
+			</form>
 		</div>
 	);
 }
--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
 import Calendar from './Calendar';
 import Scheduler from './Scheduler';
+import Modal from './Modal';
 
 function App() {
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [isModalShow, setModalShow] = useState(false);
 
-  function handleSelect(date) {
-    setSelectedDate(date);
-  }
+  const handleSelect = date => setSelectedDate(date);
+  const handleModal = message => setModalShow(true) && setModalMessage();
 
   return (
     <div className="App">
       <Calendar onSelect={handleSelect} selectedDate={selectedDate} />
       <Scheduler selectedDate={selectedDate} />
+      <Modal message={message} />
     </div>
   );
 }
