89830a84331233b40f2cd2502c46d7f56cc8b527
han-bom <livet@kakao.com>
2020-10-05 15:28:04 +0900

    모달팝업추가

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ import Modal from './Modal';
 function App() {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [isModalShow, setModalShow] = useState(false);
+  const [modalMessage, setModalMessage] = useState(false);
 
   const handleSelect = date => setSelectedDate(date);
-  const handleModal = message => setModalShow(true) && setModalMessage();
+  const handleModal = message => setModalShow(true) && setModalMessage(message);
 
   return (
     <div className="App">
       <Calendar onSelect={handleSelect} selectedDate={selectedDate} />
       <Scheduler selectedDate={selectedDate} />
-      <Modal message={message} />
+      <Modal message={modalMessage} />
     </div>
   );
 }
